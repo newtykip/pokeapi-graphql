13d2b7a502235816fe5e0e177d3770bf92cf6c3f
newt <newtykins@outlook.com>
2021-04-12 18:27:37 +0100

    contestType

--- src/data/Contests.ts
+++ src/data/Contests.ts
@@ -7,7 +7,22 @@ export default class Contests {
         this.baseURL = url;
     }
 
-    // todo: contest types
+    /**
+     * Gets information about a specified contest type.
+     * @description Contest types are categories judges used to weigh a Pokémon's condition in Pokémon contests. Check out Bulbapedia for greater detail.
+     * @param {string} name The name of the contest type
+     * @returns Info about the contest type
+     */
+    async contestType(name: string): Promise<any> {
+        const { id, name: apiName, berry_flavor, names } = await this.get(`contest-type/${name}`);
+
+        return {
+            id,
+            name: apiName,
+            berryFlavor: berry_flavor,
+            names
+        };
+    }
 
     // todo: contest effects
 
